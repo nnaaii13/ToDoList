c77388b2a0548aa56e3a6b5778549a40b66540c7
Naila <nayeli_das@hotmail.com>
2025-02-19 11:07:53 +0100

    Update main.js
    he creado localstorage. He implementado localstorage para que si se sale de la app se queden las listas guardadas. Hecho a través de la IA pero entendiendo lo que hace. Quiero que se guarden en apartados diferentes, en realizadas, no realizadas y eliminadas

--- js/main.js
+++ js/main.js
@@ -1,46 +1,56 @@
-var todos = []
+var todos = [];
+
+// Cargar la lista de tareas desde LocalStorage al iniciar
+function cargarToDos() {
+    const todosGuardados = localStorage.getItem('todos');
+    if (todosGuardados) {
+        todos = JSON.parse(todosGuardados);
+    }
+    listarToDosFn();
+}
+
+function guardarToDos() {
+    localStorage.setItem('todos', JSON.stringify(todos));
+}
 
 function crearToDoFn() {
-
     var input_crear = document.querySelector("#input_crear");
     if(input_crear.value == ""){
-        alert("Por favor ingrese la actividad que quiere realizar.")
-    }else{
-        var nuevo_todo ={
+        alert("Por favor ingrese la actividad que quiere realizar.");
+    } else {
+        var nuevo_todo = {
             titulo: input_crear.value,
             hecho: false
-        }
+        };
     
         todos.push(nuevo_todo);
         input_crear.value = '';
         listarToDosFn();
-
+        guardarToDos(); // Guardar en LocalStorage
     }
-
-    
 }
 
-function listarToDosFn(){
+function listarToDosFn() {
     var listar_todos = document.querySelector("#listar_todos");
-    listar_todos.innerHTML ="";
-    for(var i = 0; i < todos.length; i++){
+    listar_todos.innerHTML = "";
+    for(var i = 0; i < todos.length; i++) {
         var contenedor_todo = document.createElement("div");
-        contenedor_todo.className ="todo";
+        contenedor_todo.className = "todo";
         var checkbox_hecho = document.createElement("input");
         checkbox_hecho.type = "checkbox";
         checkbox_hecho.checked = todos[i].hecho;
-        checkbox_hecho.setAttribute("onChange", "marcar_hechoToDoFn("+i+")");
+        checkbox_hecho.setAttribute("onChange", "marcar_hechoToDoFn(" + i + ")");
         var spam_titulo = document.createElement("spam");
-        spam_titulo.textContent = todos[i].titulo
-        spam_titulo.className ="spam";
-        if(todos[i].hecho === true){
-            spam_titulo.className = "hecho"
+        spam_titulo.textContent = todos[i].titulo;
+        spam_titulo.className = "spam";
+        if(todos[i].hecho === true) {
+            spam_titulo.className = "hecho";
         }
         
         var buttom_eliminar = document.createElement("button");
         buttom_eliminar.textContent = "Eliminar";
-        buttom_eliminar.setAttribute("onClick","eliminar_todo("+i+")")
-        buttom_eliminar.className="boton_eliminar";
+        buttom_eliminar.setAttribute("onClick", "eliminar_todo(" + i + ")");
+        buttom_eliminar.className = "boton_eliminar";
 
         contenedor_todo.appendChild(checkbox_hecho);
         contenedor_todo.appendChild(spam_titulo);
@@ -48,21 +58,18 @@ function listarToDosFn(){
 
         listar_todos.appendChild(contenedor_todo);
     }
-
-
 }
 
-function eliminar_todo(i){
-    
-todos.splice(i, 1);
-listarToDosFn();
-
+function eliminar_todo(i) {
+    todos.splice(i, 1);
+    listarToDosFn();
+    guardarToDos(); // Guardar en LocalStorage
 }
 
-function marcar_hechoToDoFn(i){
+function marcar_hechoToDoFn(i) {
     todos[i].hecho = !todos[i].hecho;
     listarToDosFn();
-
+    guardarToDos(); // Guardar en LocalStorage
 }
 
 // Establecer fondo inicial en morado
@@ -99,5 +106,4 @@ contenedorBotones.appendChild(botonRosa);
 document.body.appendChild(contenedorBotones);
 
 // Cargar la lista al iniciar
-listarToDosFn();
-
+cargarToDos();
