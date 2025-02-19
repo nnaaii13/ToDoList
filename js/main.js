e9fb525c13e3e60d58a1bda56d4ae8908bacea56
Naila <nayeli_das@hotmail.com>
2025-02-19 10:48:09 +0100

    Update main.js
    se agregan las tareas pero no se ven, lo configuro

--- js/main.js
+++ js/main.js
@@ -1,81 +1,68 @@
-var todos = JSON.parse(localStorage.getItem('todos')) || [];
+var todos = []
 
 function crearToDoFn() {
-    var input_crear = document.querySelector("#input_crear");
-    if (input_crear.value == "") {
-        alert("Por favor ingrese la actividad que quiere realizar.");
-    } else {
-        var nuevo_todo = {
-            titulo: input_crear.value,
-            estado: "no realizada" // Estado inicial
-        };
 
+    var input_crear = document.querySelector("#input_crear");
+    if(input_crear.value == ""){
+        alert("Por favor ingrese la actividad que quiere realizar.")
+    }else{
+        var nuevo_todo ={
+            titulo: input_crear.value,
+            hecho: false
+        }
+    
         todos.push(nuevo_todo);
         input_crear.value = '';
-        guardarEnLocalStorage();
-        listarToDosFn();  // Llamamos a listarToDosFn para que se actualice inmediatamente
+        listarToDosFn();
+
     }
+
+    
 }
 
-function listarToDosFn() {
+function listarToDosFn(){
     var listar_todos = document.querySelector("#listar_todos");
-    var listar_realizadas = document.querySelector("#listar_realizadas");
-    var listar_eliminadas = document.querySelector("#listar_eliminadas");
-    listar_todos.innerHTML = "";
-    listar_realizadas.innerHTML = "";
-    listar_eliminadas.innerHTML = "";
-
-    if (todos.length === 0) {
-        listar_todos.innerHTML = "<h4>No hay To Do's creados</h4>";
-    } else {
-        for (var i = 0; i < todos.length; i++) {
-            var contenedor_todo = document.createElement("div");
-            contenedor_todo.className = "todo";
-
-            var span_titulo = document.createElement("span");
-            span_titulo.textContent = todos[i].titulo;
-            span_titulo.className = todos[i].estado;
-
-            var buttom_no_realizada = document.createElement("button");
-            buttom_no_realizada.textContent = "No Realizada";
-            buttom_no_realizada.setAttribute("onClick", "cambiarEstadoToDo(" + i + ", 'no realizada')");
-            buttom_no_realizada.className = "boton_no_realizada";
-
-            var buttom_realizada = document.createElement("button");
-            buttom_realizada.innerHTML = "&#10003;"; // Cuadrado con tick
-            buttom_realizada.setAttribute("onClick", "cambiarEstadoToDo(" + i + ", 'realizada')");
-            buttom_realizada.className = "boton_realizada";
-
-            var buttom_eliminada = document.createElement("button");
-            buttom_eliminada.innerHTML = "&#10060;"; // Cuadrado con x roja
-            buttom_eliminada.setAttribute("onClick", "cambiarEstadoToDo(" + i + ", 'eliminada')");
-            buttom_eliminada.className = "boton_eliminada";
-
-            contenedor_todo.appendChild(span_titulo);
-            contenedor_todo.appendChild(buttom_no_realizada);
-            contenedor_todo.appendChild(buttom_realizada);
-            contenedor_todo.appendChild(buttom_eliminada);
-
-            // Mostrar los elementos en la sección correspondiente
-            if (todos[i].estado === "no realizada") {
-                listar_todos.appendChild(contenedor_todo);
-            } else if (todos[i].estado === "realizada") {
-                listar_realizadas.appendChild(contenedor_todo);
-            } else if (todos[i].estado === "eliminada") {
-                listar_eliminadas.appendChild(contenedor_todo);
-            }
+    listar_todos.innerHTML ="";
+    for(var i = 0; i < todos.length; i++){
+        var contenedor_todo = document.createElement("div");
+        contenedor_todo.className ="todo";
+        var checkbox_hecho = document.createElement("input");
+        checkbox_hecho.type = "checkbox";
+        checkbox_hecho.checked = todos[i].hecho;
+        checkbox_hecho.setAttribute("onChange", "marcar_hechoToDoFn("+i+")");
+        var spam_titulo = document.createElement("spam");
+        spam_titulo.textContent = todos[i].titulo
+        spam_titulo.className ="spam";
+        if(todos[i].hecho === true){
+            spam_titulo.className = "hecho"
         }
+        
+        var buttom_eliminar = document.createElement("button");
+        buttom_eliminar.textContent = "Eliminar";
+        buttom_eliminar.setAttribute("onClick","eliminar_todo("+i+")")
+        buttom_eliminar.className="boton_eliminar";
+
+        contenedor_todo.appendChild(checkbox_hecho);
+        contenedor_todo.appendChild(spam_titulo);
+        contenedor_todo.appendChild(buttom_eliminar);
+
+        listar_todos.appendChild(contenedor_todo);
     }
+
+
 }
 
-function cambiarEstadoToDo(i, estado) {
-    todos[i].estado = estado;
-    guardarEnLocalStorage();
+function eliminar_todo(i){
+    
+todos.splice(i, 1);
+listarToDosFn();
+
+}
+
+function marcar_hechoToDoFn(i){
+    todos[i].hecho = !todos[i].hecho;
     listarToDosFn();
-}
 
-function guardarEnLocalStorage() {
-    localStorage.setItem('todos', JSON.stringify(todos));
 }
 
 // Establecer fondo inicial en morado
